6886165adfe6ee5caf991b2ebb1c5cdf2a99b9bb
K. Tremaglio <ktremaglio@gmail.com>
2023-04-28 18:46:05 -0400

    added lab files from MATLAB
    don't judge me, I'm learning

---
+++ ch201/lab9_graph_code_basic.m
@@ -0,0 +1,22 @@
+wavelength = uvdatalab9(1,:);
+a=uvdatalab9(2,:);
+b=uvdatalab9(3,:);
+c=uvdatalab9(4,:);
+d=uvdatalab9(5,:);
+e=uvdatalab9(6,:);
+f=uvdatalab9(7,:);
+g=uvdatalab9(8,:);
+
+plot(wavelength, a)
+
+hold on
+    plot(wavelength, b)
+    plot(wavelength, c)
+    plot(wavelength, d)
+    plot(wavelength, e)
+    plot(wavelength, f)
+    plot(wavelength, g)
+
+hold off
+
+
